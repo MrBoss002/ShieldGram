cbc10000ff254c288427b0a82b516f6e461ead43
Muhammed Risvan C <127328495+MrBoss002@users.noreply.github.com>
2026-09-17 14:36:41 +0400

    Implement bot setup with middlewares and handlers

---
+++ src/bot/bot.ts
@@ -0,0 +1,26 @@
+import { Bot } from "grammy";
+import { env } from "../config/env";
+import { forceSubMiddleware } from "./middlewares/forceSub";
+import { systemCleanMiddleware } from "./middlewares/systemClean";
+import { startHandler } from "./handlers/start";
+import { adminHandler } from "./handlers/admin";
+import { joinReqHandler } from "./handlers/joinReq";
+import { rulesHandler } from "./handlers/rules";
+
+// Instantiate the gramY Bot
+export const bot = new Bot(env.BOT_TOKEN);
+
+// 1. Register Middlewares (Group Message Interceptors)
+bot.use(systemCleanMiddleware);
+bot.use(forceSubMiddleware);
+
+// 2. Register Handlers & Composers
+bot.use(startHandler);
+bot.use(adminHandler);
+bot.use(joinReqHandler);
+bot.use(rulesHandler);
+
+// 3. Global Error Handler
+bot.catch((err) => {
+  console.error(`[ShieldGram Bot Error] [${err.ctx.update.update_id}]:`, err.error);
+});
